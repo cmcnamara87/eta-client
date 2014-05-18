8ea2f3e231431614516b20de31831827dd426cc4
Craig McNamara <cmcnamara87@gmail.cok>
2014-05-18 13:59:56 +1000

    Switch main module name to eta

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('test2App')
+angular.module('eta')
     .controller('SettingsCtrl', function($scope) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
--- test/spec/controllers/settings.js
+++ test/spec/controllers/settings.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: SettingsCtrl', function () {
+describe('Controller: SettingsCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('test2App'));
+    // load the controller's module
+    beforeEach(module('eta'));
 
-  var SettingsCtrl,
-    scope;
+    var SettingsCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    SettingsCtrl = $controller('SettingsCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        SettingsCtrl = $controller('SettingsCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should attach a list of awesomeThings to the scope', function() {
+        expect(scope.awesomeThings.length).toBe(3);
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
