25eaaf99547a76815a05f20440832029c30a04fd
Craig McNamara <cmcnamara87@gmail.cok>
2014-06-15 18:08:08 +1000

    Remove temp friend request items.

--- app/scripts/controllers/contacts.find.js
+++ app/scripts/controllers/contacts.find.js
@@ -2,9 +2,5 @@
 
 angular.module('etaApp')
     .controller('ContactsFindCtrl', function($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+
     });
